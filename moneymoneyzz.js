11c65a989ceecc017d7c966b34350b81fdf25f6b
JKgmr <70977623+jkgamer-web@users.noreply.github.com>
2020-10-26 15:59:51 -0600

    Update and rename .js to moneymoneyzz.js

---
+++ moneymoneyzz.js
@@ -0,0 +1,12 @@
+var money = 0;
+var moneycollect = 1;
+var randomNum = 0;
+
+alert("Welcome to moneyzmoneyz where you earn money by typing 'money', 'cash', or 'earn'");
+alert("All lower case please");
+
+function typemoney(){
+  randomNum = Math.floor(Math.random() * 3);
+  var typeforcash = prompt("Type money, cash or earn for a chance to win 1skjdfh");
+  
+}
